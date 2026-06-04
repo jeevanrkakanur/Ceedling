53fae5fcfe04bbb7b761bea4ebfa646cb498eaab
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-06-04 15:39:34 +0530

    Update test_calculator.c

--- test/test_calculator.c
+++ test/test_calculator.c
@@ -16,7 +16,7 @@ void test_add(void)
 }
 void test_subtract(void)
 {
-    TEST_ASSERT_EQUAL(2, subtract(2,3));
+    TEST_ASSERT_EQUAL(2, subtract(2,4));
     
 }
 void test_multiply(void)
@@ -26,6 +26,6 @@ void test_multiply(void)
 }
 void test_divide(void)
 {
-    TEST_ASSERT_EQUAL(2, multiply(6,3));
+    TEST_ASSERT_EQUAL(2, divide(6,3));
     
 }
