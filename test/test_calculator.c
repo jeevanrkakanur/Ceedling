db2a2d8524565827803783ffc986d8086f49b062
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-06-05 12:30:00 +0530

    Update test_calculator.c

--- test/test_calculator.c
+++ test/test_calculator.c
@@ -16,12 +16,12 @@ void test_add(void)
 }
 void test_subtract(void)
 {
-    TEST_ASSERT_EQUAL(-2, subtract(2,4));
+    TEST_ASSERT_EQUAL(2, subtract(2,4));
     
 }
 void test_multiply(void)
 {
-    TEST_ASSERT_EQUAL(6, multiply(2,3));
+    TEST_ASSERT_EQUAL(8, multiply(2,3));
     
 }
 void test_divide(void)
