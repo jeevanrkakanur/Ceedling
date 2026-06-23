7c2fce750dff00303165671f060d3d9404114366
jeevanrkakanur <jeevankakanur2445@gmail.com>
2026-06-23 23:16:18 +0530

    Update test_temperature.c

--- Test/test_temperature.c
+++ Test/test_temperature.c
@@ -15,6 +15,3 @@ void test_fahrenheit_to_celsius(void)
     TEST_ASSERT_EQUAL_FLOAT(0.0f, fahrenheit_to_celsius(32.0f));
     TEST_ASSERT_EQUAL_FLOAT(100.0f, fahrenheit_to_celsius(212.0f));
 }
-
-
-test_temperature.c
